f4e926ee0191b169ffd42f0e2629e8faa58c3442
ArunKP <arunkp1987@gmail.com>
2020-08-25 21:18:11 +0530

    Updates

---
+++ precache-manifest.2567d6a8a72cf43a9ad9821c426b5807.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968e6983e0c03413e35f09209e7ff1a9",
+    "url": "/React-Deploy/index.html"
+  },
+  {
+    "revision": "84389a0e09bbd0ca4950",
+    "url": "/React-Deploy/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1fdee2dd76923a60761d",
+    "url": "/React-Deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84389a0e09bbd0ca4950",
+    "url": "/React-Deploy/static/js/2.5e3f5be2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Deploy/static/js/2.5e3f5be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fdee2dd76923a60761d",
+    "url": "/React-Deploy/static/js/main.df4ab3cb.chunk.js"
+  },
+  {
+    "revision": "5422f807e21461521dd0",
+    "url": "/React-Deploy/static/js/runtime-main.407a03c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
